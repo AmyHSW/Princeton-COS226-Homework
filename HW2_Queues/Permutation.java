b248d13020eb5bf0f42f6f61a0dca58c614c79c4
AmyHSW <huang.shuw@husky.neu.edu>
2017-07-22 17:03:26 -0700

    HW2 - use better algorithm in Permutation.java

--- HW2_Queues/Permutation.java
+++ HW2_Queues/Permutation.java
@@ -1,15 +1,31 @@
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
         if (k < 0) throw new IllegalArgumentException("k should not be negative: " + k);
+        if (k == 0) return;
+
         RandomizedQueue<String> queue = new RandomizedQueue<String>();
-        while (!StdIn.isEmpty()) {
-            String item = StdIn.readString();
-            queue.enqueue(item);
+        for (int i = 0; i < k; i++) {
+            if (StdIn.isEmpty())
+                throw new IllegalArgumentException("Not enough values in input file");
+            queue.enqueue(StdIn.readString());
         }
+
+        int j = k;
+        while (!StdIn.isEmpty()) {
+            int r = StdRandom.uniform(++j);
+            if (r < k) {
+                queue.dequeue();
+                queue.enqueue(StdIn.readString());
+            } else {
+                StdIn.readString();
+            }
+        }
+
         for (int i = 0; i < k; i++) {
             StdOut.println(queue.dequeue());
         }
